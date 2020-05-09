57f7d104715b07916de6c6b02decdd41d3fc6b86
Aaryan-kapur <ak6392@bennett.edu.in>
2020-05-09 20:22:18 +0530

    init

---
+++ src/Table.jsx
@@ -0,0 +1,195 @@
+import React, { Component } from "react";
+
+class Row extends Component {
+  state = {};
+  componentDidMount() {
+    this.setState({
+      left: this.props.left,
+      right: this.props.right,
+      counter: this.props.counter,
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="outer">
+          <div
+            style={
+              this.state.active
+                ? {
+                    display: "flex",
+                    width: "100%",
+                    backgroundColor: "rgba(0,0,255,0.4)",
+                  }
+                : { display: "flex", width: "100%" }
+            }
+            className="OuterRow"
+          >
+            <div
+              style={{
+                textAlign: "center",
+                width: "4%",
+                padding: "10px 0",
+                border: "1px solid black",
+              }}
+              className="counter"
+            >
+              {this.state.counter}
+            </div>
+            <div
+              style={{
+                width: "48%",
+                padding: "10px",
+                border: "1px solid black",
+              }}
+              className="leftcont"
+            >
+              {this.state.left}
+            </div>
+            <div
+              style={{
+				width: "48%",
+                padding: "10px",
+                border: "1px solid black",
+              }}
+              className="rightcont"
+            >
+              <textarea
+                style={
+                  this.state.active
+                    ? {
+						
+                        width: "100%",
+                        padding: "10px",
+                        height: "100%",
+                        outline: "none",
+                        border: "none",
+                        backgroundColor: "rgba(0,0,255,0)",
+                      }
+                    : {
+						width: "100%",
+						borderRightColor : "black",
+						borderRightStyle: "solid",
+						borderRightWidth: "2px",
+                        // padding: "10px",	
+                        height: "100%",
+                        outline: "none",
+                        border: "none",
+                      }
+                }
+                type="text"
+                onBlurCapture={() => this.setState({ active: false })}
+                onFocusCapture={() => this.setState({ active: true })}
+              />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default class SplitText extends Component {
+  constructor() {
+    super();
+    this.update = this.update.bind(this);
+    this.state = {
+      paragraphs: [""],
+      inputarea: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ornare odio non scelerisque finibus. Donec at erat ac libero finibus fermentum id lacinia orci. Suspendisse interdum libero mollis, vulputate lectus laoreet, vehicula quam. Vestibulum velit felis, consequat ac ullamcorper vitae, malesuada nec mi. Maecenas venenatis ligula vel facilisis semper. Vestibulum rhoncus purus et facilisis rhoncus. Quisque eu sapien feugiat tellus interdum maximus id eget turpis. Ut laoreet ipsum nisi, eget vehicula tellus aliquet eget. Donec euismod lorem et vestibulum faucibus. Fusce euismod tempor diam, volutpat molestie nibh sollicitudin vitae. Cras nec finibus nisl.
+
+Nullam et nisi tortor. Maecenas euismod nunc aliquet urna blandit laoreet. Praesent ut tortor nisi. Integer et ex mauris. Quisque eu metus sed dui viverra molestie et eget libero. Sed egestas leo quis sem ultricies mollis. Fusce eget efficitur tellus, iaculis molestie nisi.
+        
+Ut vestibulum ornare malesuada. Fusce tristique euismod nisi ac dapibus. Proin arcu sapien, pharetra et lacus non, posuere molestie elit. Quisque venenatis nibh non elit porta auctor. Aenean quis justo sem. Fusce suscipit nibh eu cursus varius. Vivamus facilisis sed odio sit amet tincidunt. Cras fringilla massa pharetra orci tincidunt, at molestie nunc dapibus. Cras at sem dolor. Vestibulum luctus neque ac mauris ultrices iaculis. Ut ac tempor nulla. Suspendisse malesuada sem ut bibendum sagittis. Maecenas ultrices est ac urna dictum, a blandit dui porttitor. Donec elit eros, malesuada at nunc a, faucibus vestibulum nisi.
+        
+Curabitur eget tempor urna, bibendum aliquet ipsum. Nullam viverra, mauris ut efficitur mollis, odio sem viverra metus, fermentum condimentum libero turpis vel magna. In et ultricies mi. Morbi vestibulum, purus sit amet hendrerit commodo, augue metus maximus mi, eget condimentum dui orci a ante. Nam pretium turpis lacus, tempus pretium mi eleifend at. Cras nec leo et elit fringilla accumsan sit amet et lectus. Pellentesque vitae ante sem. In hac habitasse platea dictumst.
+        
+Curabitur iaculis quis erat quis feugiat. Pellentesque metus purus, scelerisque tempus ullamcorper id, consequat a elit. Praesent efficitur vehicula tempus. Pellentesque in ultricies enim, a molestie massa. Vivamus vitae consectetur risus. In vel ullamcorper augue, tincidunt efficitur ipsum. Cras cursus sapien nec laoreet tempus.
+        
+Cras augue ipsum, finibus eu feugiat in, vehicula eget nunc. Mauris interdum purus eu velit euismod tristique. Etiam ultrices ut nunc vel tincidunt. Etiam eu dui sem. Fusce eget tortor in elit vestibulum congue quis vitae ligula. Nam interdum, purus vel facilisis dignissim, ex ligula cursus metus, sit amet posuere leo sapien non eros. Integer efficitur, purus quis ornare aliquam, nulla dui dictum tellus, ut vestibulum nisi massa nec est. Cras malesuada vitae dui in ullamcorper. Quisque dictum interdum nunc, ut mollis eros ullamcorper eu. Sed viverra, libero id laoreet rhoncus, nibh leo egestas ligula, ac congue ipsum tellus et mauris.
+        
+Cras libero turpis, convallis pellentesque viverra consequat, tristique vitae quam. Vivamus ipsum tellus, fermentum quis massa at, consequat auctor nulla. Nulla ultricies nunc ut posuere volutpat. Aliquam cursus, justo varius efficitur cursus, eros nisl faucibus ante, eu congue lectus lectus fermentum lorem. Ut eget scelerisque urna. Pellentesque maximus felis ac turpis faucibus, dapibus pharetra tellus aliquet. Aliquam vel commodo enim. Sed placerat neque elit, sit amet pellentesque odio maximus ut. Duis sollicitudin ullamcorper odio eu bibendum. Aliquam non feugiat mauris.
+        
+Mauris fermentum purus tellus, eu volutpat turpis viverra in. Proin pulvinar urna ac mauris luctus, vitae accumsan dui rhoncus. Duis lobortis pellentesque arcu, at egestas nisi tincidunt nec. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer imperdiet venenatis est. Praesent id tincidunt diam. Pellentesque facilisis nec massa in laoreet. Phasellus ut leo tincidunt massa vestibulum condimentum eu eget turpis. Duis quis ex consectetur, mollis orci id, varius nisl. Mauris porttitor, eros et molestie luctus, velit tellus consequat enim, vitae volutpat nisl odio quis neque. Mauris tincidunt scelerisque enim sed tempus. Duis sit amet enim libero. Proin scelerisque dolor diam, ornare varius nulla mollis at. Duis blandit vestibulum sem, id mollis libero vehicula eget. Aliquam accumsan vel nisl id venenatis. Maecenas vitae lectus efficitur, sagittis nunc volutpat, dapibus mi.
+        
+Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum dapibus est in odio luctus varius. Vivamus a eros quam. Suspendisse in felis in neque sagittis viverra. Duis vestibulum et lorem non egestas. Nam efficitur nisl non tellus cursus feugiat. Suspendisse sapien nisi, interdum quis ex id, malesuada commodo lorem. Aliquam lobortis mauris quis tortor imperdiet, eget mollis quam feugiat. Etiam in tellus ex. Cras molestie lacinia vulputate. Nulla et elit volutpat, hendrerit ipsum sit amet, dapibus lorem.
+        
+Proin quis molestie turpis. Etiam bibendum lobortis mauris, sit amet posuere purus tempus quis. Suspendisse auctor, nunc eget ornare vulputate, quam sapien dapibus sem, rutrum sagittis velit turpis sed augue. Maecenas convallis nulla lobortis, tempor turpis sit amet, pharetra purus. Sed commodo placerat fringilla. In hac habitasse platea dictumst. Proin pharetra et ex a fringilla. Phasellus eros justo, viverra sit amet rhoncus at, dictum id lectus. Nam tincidunt eros non sagittis blandit. Nunc in venenatis erat. Vestibulum dictum velit sit amet commodo ornare.`,
+      count: 0,
+      convert: [],
+    };
+  }
+
+  componentDidMount() {
+    // document.getElementById('after').style.display = 'none'
+  }
+
+  update() {
+    let paragraphs = this.state.inputarea.trim().split("\n");
+
+    this.setState({ paragraphs: paragraphs });
+    let convert = [];
+    paragraphs.forEach((element) => {
+      let paragraph = { lines: [] };
+      let lines = element.trim().split(".");
+      lines.forEach((line) => {
+        line = line.trim();
+        if (line !== "") paragraph.lines.push(line);
+      });
+      if (paragraph.lines.length > 0) convert.push(paragraph);
+    });
+    this.setState({ convert: convert });
+    document.getElementById("after").style.display = "block";
+    document.getElementById("before").style.display = "none";
+    document.getElementById("before").style.visibility = "hidden";
+    this.forceUpdate();
+
+    // const row = document.createElement('Row')
+    // row.left = 'hello'
+    // document.getElementById('after').appendChild(row)
+  }
+
+  render() {
+    return (
+      <React.Fragment >
+		  <div className="outerPehle">
+
+		
+        <div id="before">
+          <textarea
+            name="inputarea"
+            id="inputarea"
+            cols="60"
+            rows="40"
+            onChange={(e) => this.setState({ inputarea: e.target.value })}
+            value={this.state.inputarea}
+          ></textarea>
+          <br />
+          <input type="submit" value="Submit" onClick={() => this.update()} />
+        </div>
+        <div id="after" className="after">
+          {this.state.convert.map((elem) => {
+            let counter = 0;
+            return elem.lines.map((line) => {
+              counter += 1;
+              if (counter === 1)
+                return (
+                  <React.Fragment>
+                    <br />
+                    <Row counter={counter} left={line}></Row>
+                  </React.Fragment>
+                );
+              return <Row counter={counter} left={line}></Row>;
+            });
+          })}
+        </div>
+        {/* {this.state.convert === [] ? (
+					''
+				) : (
+					<DisplayText convert={this.state.convert}></DisplayText>
+				)} */}
+				</div>
+      </React.Fragment>
+    );
+  }
+}
