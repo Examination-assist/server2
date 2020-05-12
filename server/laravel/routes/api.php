6fa94b69425f76c773fe3a200f2ae6eb9951487f
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-05-12 05:51:14 +0530

    all done

--- server/laravel/routes/api.php
+++ server/laravel/routes/api.php
@@ -13,32 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-class DocxConversion{
+class DocxConversion
+{
     private $filename;
 
-    public function __construct($filePath) {
+    public function __construct($filePath)
+    {
         $this->filename = $filePath;
     }
 
-    private function read_doc() {
+    private function read_doc()
+    {
         $fileHandle = fopen($this->filename, "r");
-        $line = @fread($fileHandle, filesize($this->filename));   
-        $lines = explode(chr(0x0D),$line);
+        $line = @fread($fileHandle, filesize($this->filename));
+        $lines = explode(chr(0x0D), $line);
         $outtext = "";
-        foreach($lines as $thisline)
-          {
+        foreach ($lines as $thisline) {
             $pos = strpos($thisline, chr(0x00));
-            if (($pos !== FALSE)||(strlen($thisline)==0))
-              {
-              } else {
-                $outtext .= $thisline." ";
-              }
-          }
-         $outtext = preg_replace("/[^a-zA-Z0-9\s\,\.\-\n\r\t@\/\_\(\)]/","",$outtext);
+            if (($pos !== FALSE) || (strlen($thisline) == 0)) {
+            } else {
+                $outtext .= $thisline . " ";
+            }
+        }
+        $outtext = preg_replace("/[^a-zA-Z0-9\s\,\.\-\n\r\t@\/\_\(\)]/", "", $outtext);
         return $outtext;
     }
 
-    private function read_docx(){
+    private function read_docx()
+    {
 
         $striped_content = '';
         $content = '';
@@ -56,7 +58,7 @@ class DocxConversion{
             $content .= zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
 
             zip_entry_close($zip_entry);
-        }// end while
+        } // end while
 
         zip_close($zip);
 
@@ -67,67 +69,69 @@ class DocxConversion{
         return $striped_content;
     }
 
- /************************excel sheet************************************/
+    /************************excel sheet************************************/
 
-function xlsx_to_text($input_file){
-    $xml_filename = "xl/sharedStrings.xml"; //content file name
-    $zip_handle = new ZipArchive;
-    $output_text = "";
-    if(true === $zip_handle->open($input_file)){
-        if(($xml_index = $zip_handle->locateName($xml_filename)) !== false){
-            $xml_datas = $zip_handle->getFromIndex($xml_index);
-            $xml_handle = DOMDocument::loadXML($xml_datas, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
-            $output_text = strip_tags($xml_handle->saveXML());
-        }else{
-            $output_text .="";
+    function xlsx_to_text($input_file)
+    {
+        $xml_filename = "xl/sharedStrings.xml"; //content file name
+        $zip_handle = new ZipArchive;
+        $output_text = "";
+        if (true === $zip_handle->open($input_file)) {
+            if (($xml_index = $zip_handle->locateName($xml_filename)) !== false) {
+                $xml_datas = $zip_handle->getFromIndex($xml_index);
+                $xml_handle = DOMDocument::loadXML($xml_datas, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
+                $output_text = strip_tags($xml_handle->saveXML());
+            } else {
+                $output_text .= "";
+            }
+            $zip_handle->close();
+        } else {
+            $output_text .= "";
         }
-        $zip_handle->close();
-    }else{
-    $output_text .="";
+        return $output_text;
     }
-    return $output_text;
-}
 
-/*************************power point files*****************************/
-function pptx_to_text($input_file){
-    $zip_handle = new ZipArchive;
-    $output_text = "";
-    if(true === $zip_handle->open($input_file)){
-        $slide_number = 1; //loop through slide files
-        while(($xml_index = $zip_handle->locateName("ppt/slides/slide".$slide_number.".xml")) !== false){
-            $xml_datas = $zip_handle->getFromIndex($xml_index);
-            $xml_handle = DOMDocument::loadXML($xml_datas, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
-            $output_text .= strip_tags($xml_handle->saveXML());
-            $slide_number++;
+    /*************************power point files*****************************/
+    function pptx_to_text($input_file)
+    {
+        $zip_handle = new ZipArchive;
+        $output_text = "";
+        if (true === $zip_handle->open($input_file)) {
+            $slide_number = 1; //loop through slide files
+            while (($xml_index = $zip_handle->locateName("ppt/slides/slide" . $slide_number . ".xml")) !== false) {
+                $xml_datas = $zip_handle->getFromIndex($xml_index);
+                $xml_handle = DOMDocument::loadXML($xml_datas, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
+                $output_text .= strip_tags($xml_handle->saveXML());
+                $slide_number++;
+            }
+            if ($slide_number == 1) {
+                $output_text .= "";
+            }
+            $zip_handle->close();
+        } else {
+            $output_text .= "";
         }
-        if($slide_number == 1){
-            $output_text .="";
-        }
-        $zip_handle->close();
-    }else{
-    $output_text .="";
+        return $output_text;
     }
-    return $output_text;
-}
 
 
-    public function convertToText() {
+    public function convertToText()
+    {
 
-        if(isset($this->filename) && !file_exists($this->filename)) {
+        if (isset($this->filename) && !file_exists($this->filename)) {
             return "File Not exists";
         }
 
         $fileArray = pathinfo($this->filename);
         $file_ext  = $fileArray['extension'];
-        if($file_ext == "doc" || $file_ext == "docx" || $file_ext == "xlsx" || $file_ext == "pptx")
-        {
-            if($file_ext == "doc") {
+        if ($file_ext == "doc" || $file_ext == "docx" || $file_ext == "xlsx" || $file_ext == "pptx") {
+            if ($file_ext == "doc") {
                 return $this->read_doc();
-            } elseif($file_ext == "docx") {
+            } elseif ($file_ext == "docx") {
                 return $this->read_docx();
-            } elseif($file_ext == "xlsx") {
+            } elseif ($file_ext == "xlsx") {
                 // return $this->xlsx_to_text();
-            }elseif($file_ext == "pptx") {
+            } elseif ($file_ext == "pptx") {
                 // return $this->pptx_to_text();
             }
         } else {
@@ -144,17 +148,61 @@ Route::post('/upload', function (Request $request) {
     $request->validate([
         'file' => 'required', //mimes:pdf,xlx,csv|max:2048',
     ]);
-    
+
 
     $fileName = time() . '.' . $request->filename;
     $request->file->move(public_path('uploads'), $fileName);
-    
-    $paths = public_path('uploads').$fileName;
-    $docObj = new DocxConversion(public_path('uploads').'/'.$fileName);
-    
+
+    $paths = public_path('uploads') . $fileName;
+    $docObj = new DocxConversion(public_path('uploads') . '/' . $fileName);
+
     // Log::info();
     // return 'ok';
-    return response()->json(['data'=>'ok','file'=>$fileName,'text'=>$docObj->convertToText(),'paths'=>$paths]);
-        // ->with('success', 'You have successfully upload file.');
+    return response()->json(['data' => 'ok', 'file' => $fileName, 'text' => $docObj->convertToText(), 'paths' => $paths]);
+    // ->with('success', 'You have successfully upload file.');
     // ->with('file', $fileName);
+});
+
+Route::post('/convert', function (Request $request) {
+    $phpWord_s = new \PhpOffice\PhpWord\PhpWord();
+    $phpWord_both = new \PhpOffice\PhpWord\PhpWord();
+
+    $section_s = $phpWord_s->addSection();
+    $section_both = $phpWord_both->addSection();
+
+    $s = "";
+    $s1 = "";
+    $input = json_decode($request->input);
+    $output = json_decode($request->output);
+    for ($para = 0; $para < count($input); $para++) {
+        for ($line = 0; $line < count($input[$para]); $line++) {
+            $s = $s . $input[$para][$line];
+            $s = $s . "\n";
+            $s = $s . $output[$para][$line];
+            $s = $s . "\n";
+            $s = $s . "\n";
+
+            $s1 = $s1 . $output[$para][$line];
+            $s1 = $s1 . "\n";
+        }
+        $s = $s . "\n";
+        $s1 = $s1 . "\n";
+    }
+
+    $section_s->addText($s1);
+    $section_both->addText($s);
+
+    $objWriter_s = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord_s, 'Word2007');
+    $objWriter_both = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord_both, 'Word2007');
+
+    $time = time();
+
+    $objWriter_s->save(public_path('word_files') . '/' . 's' . $time . '.docx');
+    $objWriter_both->save(public_path('word_files') . '/' . 'both' . $time . '.docx');
+
+    return response()->json([
+        's' => $s,
+        'path_s' => public_path('word_files') . '/' . 's' . $time . '.docx',
+        'path_both' => public_path('word_files') . '/' . 'both' . $time . '.docx'
+    ]);
 });
