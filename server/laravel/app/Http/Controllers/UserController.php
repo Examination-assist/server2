09d0e54db4f350ff457aa85388ceedb29db6e150
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-05-16 12:12:30 +0530

    reg

--- server/laravel/app/Http/Controllers/UserController.php
+++ server/laravel/app/Http/Controllers/UserController.php
@@ -2,19 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\UserType;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+
 use Illuminate\Support\Facades\Crypt;
 
 class UserController extends Controller
 {
     function register(Request $request){
         $validated=$request->validate([
-            'username'=>'required',
+            'email'=>'required',
             'password'=>'required',
-            'user_type'=>''
+            'user_type'=>'required'
         ]);
-
+        
+        // Log::info($validated->user_type);
+        $model = UserType::where('user_type','Admin');
+        Log::info($model);
+        // UserType::
         // Log::info(Crypt::encryptString($request->user));
         // Log::info(Crypt::decryptString(Crypt::encryptString($request->user)));
 
--- server/laravel/app/UserType.php
+++ server/laravel/app/UserType.php
@@ -8,4 +8,6 @@ class UserType extends Model
 {
     //
     protected $table='user_types';
+
+    
 }
