fdecb283bd6f14c89a4c8dafcf603e7bae5c6444
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-05-16 12:12:30 +0530

    reg

--- client/src/scripts/Registration/Register.jsx
+++ client/src/scripts/Registration/Register.jsx
@@ -4,10 +4,12 @@ import axios from 'axios'
 export default class Login extends Component {
 	async componentDidMount() {
 		console.log(await axios.post('http://localhost:8000/api/register', {
-			username: 'user',
+			first_name: 'Aniket',
+			last_name: 'Chowdhury',
+			username: 'aniketbiprojit',
 			password: 'password',
 			user_type: 'Translator',
-			email:'email@mail.com'
+			email:'8.aniket.chowdhury@gmail.com'
 		}))
 	}
 	render() {
