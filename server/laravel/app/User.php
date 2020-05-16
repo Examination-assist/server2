1ded7d501c5c73a7059454eb2ff0e065d9030f55
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-05-16 11:17:39 +0530

    User model

--- server/laravel/app/User.php
+++ server/laravel/app/User.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     //
+    protected $table='users';
+    protected $id='user_id';
+    public $incrementing = true;
+    public $timestamps = true;
+
 }
