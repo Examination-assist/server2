de142ff4adf808b36d00beee12ac24fae2b96447
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-05-16 13:38:39 +0530

    pages

--- client/src/scripts/Registration/Register.jsx
+++ client/src/scripts/Registration/Register.jsx
@@ -2,17 +2,85 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 export default class Register extends Component {
-	async componentDidMount() {
-		console.log(await axios.post('http://localhost:8000/api/register', {
-			first_name: 'Aniket',
-			last_name: 'Chowdhury',
-			username: 'aniketbiprojit',
-			password: 'password',
-			user_type: 'Translator',
-			email:'8.aniket.chowdhury@gmail.com'
-		}))
+	constructor() {
+		super()
+		this.handleChange = this.handleChange.bind(this)
+		this.post = this.post.bind(this)
 	}
+	handleChange(e) {
+		this.setState({ [e.target.name]: e.target.value })
+	}
+	async componentDidMount() {
+		this.setState({
+			first_name: '',
+			last_name: '',
+			username: '',
+			password: '',
+			user_type: 'Translator',
+			email: '',
+		})
+	}
+
+	async post() {
+		const res = await axios.post('http://localhost:8000/api/register', {
+			first_name: this.state.first_name,
+			last_name: this.state.last_name,
+			username: this.state.username,
+			password: this.state.password,
+			user_type: this.state.user_type,
+			email: this.state.email,
+		})
+		console.log(res)
+		if(res.status===200){
+			this.setState({success:true})
+		}
+	}
+
 	render() {
-		return <div></div>
+		return (
+			<div>
+				<form method='post'>
+					<br/>
+					<input
+						onChange={this.handleChange}
+						placeholder='email'
+						type='email'
+						name='email'
+						required
+					/>
+					<br/>
+					<input
+						onChange={this.handleChange}
+						placeholder='first_name'
+						type='text'
+						name='first_name'
+					/>
+					<br/>
+					<input
+						onChange={this.handleChange}
+						placeholder='last_name'
+						type='text'
+						name='last_name'
+					/>
+					<br/>
+					<input
+						onChange={this.handleChange}
+						placeholder='username'
+						type='text'
+						name='username'
+					/>
+					<br/>
+					<input
+						onChange={this.handleChange}
+						placeholder='password'
+						type='password'
+						name='password'
+						required
+					/>
+					<br/>
+					<input type="button" value="Submit" onClick={this.post}/>
+				</form>
+			</div>
+		)
 	}
 }
