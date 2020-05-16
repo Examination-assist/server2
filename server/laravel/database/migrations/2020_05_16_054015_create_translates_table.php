bcdfc9d409b0387da6d9f32ad2a5b95a5833073f
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-05-16 11:16:46 +0530

    translates

---
+++ server/laravel/database/migrations/2020_05_16_054015_create_translates_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTranslatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('translates', function (Blueprint $table) {
+            $table->id('translate_id');
+            $table->timestamps();
+
+            $table->integer('line_counter');
+
+            $table->integer('para');
+            $table->integer('count');
+            
+            $table->mediumText('input')->nullable()->charset('utf8')->collation('utf8_unicode_ci');
+            $table->mediumText('output')->nullable()->charset('utf8')->collation('utf8_unicode_ci');
+            
+            $table->foreignId('doc_id')->references('doc_id')->on('documents');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('translates');
+    }
+}
