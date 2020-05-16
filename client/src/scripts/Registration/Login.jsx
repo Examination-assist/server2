a5781b9fc54c376cb9957dae602c09dfb7c26c9d
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-05-16 23:10:20 +0530

    docs-update

--- client/src/scripts/Registration/Login.jsx
+++ client/src/scripts/Registration/Login.jsx
@@ -15,8 +15,8 @@ export default class Login extends Component {
 
 	async componentDidMount() {
 		this.setState({
-			email: '8.aniket.chowdhury@gmail.com',
-            password: 'password',
+			email: '',
+            password: '',
 		})
 	}
 
@@ -32,7 +32,11 @@ export default class Login extends Component {
 			localStorage.setItem('email', res.data.email)
 			localStorage.setItem('user_id', res.data.user_id)
 			this.setState({ success: true })
-		}
+        }
+        if(res.status === 400){
+            console.log(res)
+            this.setState({err:res})
+        }
 	}
 
 	render() {
