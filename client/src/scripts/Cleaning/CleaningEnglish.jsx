b55d4b66f4a179cff24ba23bc9078ce41e202b52
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-06-18 18:37:30 +0530

    feat:clean

--- client/src/scripts/Cleaning/CleaningEnglish.jsx
+++ client/src/scripts/Cleaning/CleaningEnglish.jsx
@@ -5,43 +5,74 @@ import axios from 'axios'
 const ENDPOINT = require('../Registration/config')
 
 class CleaningEnglish extends Component {
-	state = { discipline: [], course: [], users: [] }
+	state = {
+		courses: [],
+		users: [],
+		all_data: [],
+		lectures: [],
+		ytd: 'tgbNymZ7vqY',
+	}
 	async componentDidMount() {
-		const discipline = await axios.post(ENDPOINT + 'courses?get=discipline')
-		console.log(discipline)
-		this.setState({ discipline: discipline.data })
+		const all_data = await axios.post(ENDPOINT + 'links')
+		console.log(all_data.data[0].course_name)
+		this.setState({ all_data: all_data.data })
 
-		const course = await axios.post(
-			ENDPOINT + 'courses?get=course_name&discipline=' + 'BASIC SCIENCE'
-		)
-		this.setState({ course: course.data })
+		let courses = []
 
-		this.handleSubmit()
+		this.state.all_data.forEach((element) => {
+			// console.log(element.course_name)
+			if (!courses.includes(element.course_name))
+				courses.push(element.course_name)
+		})
+		console.log(courses)
+		this.setState({ courses: courses })
+
+		// const course = await axios.post(
+		// 	ENDPOINT + 'courses?get=course_name&discipline=' + 'BASIC SCIENCE'
+		// )
+		// this.setState({ course: course.data })
+
+		// this.handleSubmit()
 	}
 
 	async execute(e) {
-		const disp = e.target.value
-		const course = await axios.post(
-			ENDPOINT + 'courses?get=course_name&discipline=' + disp
-		)
-		this.setState({ course: course.data })
-	}
-
-	async handleSubmit() {
-		const elems = []
-		Array.prototype.slice
-			.call(document.querySelectorAll('select'))
-			.map((e) => {
-				elems.push(e.value)
-			})
-
-		const data = await axios.post(ENDPOINT + 'get_user_data', {
-			discipline: elems[0],
-			course_name: elems[1],
-			language: elems[2],
+		let lectures = []
+		const course = e.target.value
+		this.state.all_data.forEach((element) => {
+			if (element.course_name === course) {
+				lectures.push(element)
+			}
 		})
-		this.setState({ users: data.data })
+		console.log(lectures[0])
+		this.setState({ lectures: lectures })
+		this.state.lectures.map((elem) => console.log(elem.lectureId))
 	}
+
+	async YTD(e) {
+		let lec_id = e.target.value
+		this.state.lectures.forEach((elem) => {
+			if (lec_id === elem.lectureID) {
+				this.setState({ ytd: elem.YouTubeId })
+			}
+		})
+		this.forceUpdate()
+	}
+
+	// async handleSubmit() {
+	// 	const elems = []
+	// 	Array.prototype.slice
+	// 		.call(document.querySelectorAll('select'))
+	// 		.map((e) => {
+	// 			elems.push(e.value)
+	// 		})
+
+	// 	const data = await axios.post(ENDPOINT + 'get_user_data', {
+	// 		discipline: elems[0],
+	// 		course_name: elems[1],
+	// 		language: elems[2],
+	// 	})
+	// 	this.setState({ users: data.data })
+	// }
 	render() {
 		return (
 			<React.Fragment>
@@ -49,37 +80,28 @@ class CleaningEnglish extends Component {
 					<h2> English Cleaning</h2>
 					<div class='dropdown'>
 						<select
+							onChange={(e) => this.execute(e)}
 							class='dropbtn'
-							name='Discipline'
-							id='Discipline'
-							onClick={(e) => this.execute(e)}
+							name='Course'
+							id='Course'
 						>
-							{this.state.discipline.map((elem) => (
-								<option value={elem.discipline}>
-									{elem.discipline}
-								</option>
-							))}
-						</select>{' '}
-					</div>
-					<div class='dropdown'>
-						<select class='dropbtn' name='Course' id='Course'>
-							{this.state.course.map((elem) => (
-								<option value={elem.course_name}>
-									{elem.course_name}
-								</option>
+							{this.state.courses.map((elem) => (
+								<option value={elem}>{elem}</option>
 							))}
 						</select>
 					</div>
 					<div class='dropdown'>
-						<select n class='dropbtn' name='Language' id='Language'>
-							<option value='Hindi'>Hindi</option>
-							<option value='Bengali'>Bengali</option>
-							<option value='Marathi'>Marathi</option>
-							<option value='Telugu'>Telugu</option>
-							<option value='Tamil'>Tamil</option>
-							<option value='Gujarati'>Gujarati</option>
-							<option value='Kannada'>Kannada</option>
-							<option value='Malayalam'>Malayalam</option>
+						<select
+							onChange={(e) => this.YTD(e)}
+							class='dropbtn'
+							name='lecture_name'
+							id='lecture_name'
+						>
+							{this.state.lectures.map((elem) => (
+								<option value={elem.lectureId}>
+									{elem.lectureId}
+								</option>
+							))}
 						</select>
 					</div>
 					<br />
@@ -95,12 +117,13 @@ class CleaningEnglish extends Component {
 					</button>
 					<br></br>
 					<br></br>
-
-					<iframe
-						width='540'
-						height='315'
-						src='https://www.youtube.com/embed/tgbNymZ7vqY'
-					></iframe>
+					<div id='if-outer'>
+						<iframe
+							width='540'
+							height='315'
+							src={`https://www.youtube.com/embed/${this.state.ytd}`}
+						></iframe>
+					</div>
 
 					<div className='outer'>
 						<div className='title' style={{ textAlign: 'center' }}>
